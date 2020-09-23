eabc17772ef8d105918c9a641a85554c886a6266
Christian <christian@schladetsch.com>
2020-09-24 02:59:42 +1000

    Add preliminary tests for continuation replacement.

--- scripts/TestContinuation.f
+++ scripts/TestContinuation.f
@@ -3,16 +3,21 @@
 #
 # Make a 'function' that just returns 1
 {1} 'a= a& 1 == assert
+
 # Make a function that adds whatever two things are on the stack
 {+} 'b=
 a& 2 b& 3 == assert
+
 # Make a continuation that doubles what's on the stack
 { 2 * } 'd =
 2 d& 4 == assert
 2 d& d& 8 == assert
+
 # Can also use locals in continuations.
 # This stores whatever is on the stack to a local called 'a',
 # then uses it to calculate 'a*2 + a'
 { 'a= a 2 * a + } 'e =
 3 e& 9 == assert
+
 depth 0 == assert
+"Done" print
